bd9a4066664db0cb453fc16cf1edc5066eb25dd7
Saubhagya Pandey <saubhagyapandey20@gmail.com>
2023-04-06 02:59:02 +0530

    added icons

--- lib/main.dart
+++ lib/main.dart
@@ -35,6 +35,26 @@ class QuizPage extends StatefulWidget {
 class _QuizPageState extends State<QuizPage> {
   List<Icon> scoreKeeper = [];
 
+  void checkAnswer(bool userPickedAnswer) {
+    bool correctanswers = quizBrain.getCorrectAnswer();
+
+    setState(() {
+      if (userPickedAnswer == correctanswers) {
+        scoreKeeper.add(Icon(
+          Icons.check,
+          color: Colors.green,
+        ));
+      } else {
+        scoreKeeper.add(Icon(
+          Icons.close,
+          color: Colors.red,
+        ));
+      }
+
+      quizBrain.nextQuestion();
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -73,17 +93,7 @@ class _QuizPageState extends State<QuizPage> {
               onPressed: () {
                 //The user picked true.
 
-                bool correctanswers = quizBrain.getCorrectAnswer();
-
-                if (correctanswers == true) {
-                  print('user got it right');
-                } else {
-                  print('user got it wrong');
-                }
-
-                setState(() {
-                  quizBrain.nextQuestion();
-                });
+                checkAnswer(true);
               },
             ),
           ),
@@ -105,17 +115,7 @@ class _QuizPageState extends State<QuizPage> {
               onPressed: () {
                 //The user picked false.
 
-                bool correctanswers = quizBrain.getCorrectAnswer();
-
-                if (correctanswers == false) {
-                  print('user got it right');
-                } else {
-                  print('user got it wrong');
-                }
-
-                setState(() {
-                  quizBrain.nextQuestion();
-                });
+                checkAnswer(false);
               },
             ),
           ),
